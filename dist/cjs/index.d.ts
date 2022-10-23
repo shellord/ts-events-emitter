3867732e1fff0d9a1cc1c534b8639c41e05f62b9
Saheen Shoukath <saheenshoukath@gmail.com>
2022-10-23 23:27:52 +0530

    remove dist

--- dist/cjs/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare type TMessage = unknown;
-declare const eventsEmitter: <T extends string>() => {
-    on: (event: T, callback: (message: TMessage) => void) => void;
-    emit: (event: T, message: TMessage) => void;
-    once: (event: T, callback: (message: TMessage) => void) => void;
-    off: (event: T, callback: (message: TMessage) => void) => void;
-    removeAllListeners: (event: T) => void;
-};
-export default eventsEmitter;
